b3a756c61c8df87fbc641f801a4f1bb718e48ba5
Mouhammad Anas Alatasi <anas.alatasi.pc@gmail.com>
2021-07-10 14:33:55 +0300

    Added BottomBar and floatingButton without themes

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,26 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+            )
+          ],
           elevation: 15.0,
           backgroundColor: Color(0xFF9a0404),
           title: Text('Name'),
@@ -19,6 +33,35 @@ class MyApp extends StatelessWidget {
           child: ListView(
             children: [],
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: (value) {
+            setState(() {
+              print(_selectedIndex);
+              _selectedIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'settings',
+            ),
+            
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed : (){},
+          child: Icon(Icons.auto_fix_high_outlined),
+
         ),
       ),
     );
