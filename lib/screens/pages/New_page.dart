492ffe8ff2343d22622c30e76c7fd35d03e85991
Moussa Syoufi <mousasya3@gmail.com>
2021-07-19 14:28:29 +0300

    trying to add pull to refresh functionality

--- lib/screens/pages/New_page.dart
+++ lib/screens/pages/New_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:jallaliefern_taking_orders_app/orders_list.dart';
+import 'package:jallaliefern_taking_orders_app/models/order.dart';
+import 'package:jallaliefern_taking_orders_app/widgets/orders_list.dart';
 import 'package:jallaliefern_taking_orders_app/services/api_service.dart';
 import 'package:jallaliefern_taking_orders_app/utils/service_locator.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
+class NewPage extends StatefulWidget {
+  @override
+  _NewPageState createState() => _NewPageState();
+}
 
-class NewPage extends StatelessWidget {
+class _NewPageState extends State<NewPage> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: true);
+  List<Order> orders = List<Order>.empty();
+  void _onRefresh() async {
+    try {
+      final tmp = await locator<ApiService>().getNewOrders();
+      setState(() {orders = tmp;});
+      _refreshController.refreshCompleted();
+    } catch (e) {
+      _refreshController.refreshFailed();
+    }
+  }
 
   @override
-  Widget build(BuildContext context) => FutureBuilder(
-    future: locator<ApiService>().getNewOrders(),
-    builder: (BuildContext context,AsyncSnapshot snapshot) {
-        if (snapshot.hasData) return OrdersList(orders: snapshot.data);
-        return Container();
-    });
+  Widget build(BuildContext context) => SmartRefresher(enablePullDown: true,controller: _refreshController,onRefresh: _onRefresh,child: OrdersList(orders: orders));
 }
