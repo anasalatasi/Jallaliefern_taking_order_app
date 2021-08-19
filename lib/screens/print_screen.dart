e560cd49c4c6859652ad721bbab295fc26eb03cd
mousasya3 <mousasya3@gmail.com>
2021-08-19 12:06:53 +0300

    Revert "Merge branch 'master' of https://gitlab.com/jallaliefern/jallaliefern_taking_orders_app"
    This reverts commit 6a8a2d869119588cb4a6e07801599c031c1a1dcf, reversing
    changes made to 098b27a37207b974a6b34def6164bd5ff4138136.

--- lib/screens/print_screen.dart
+++ lib/screens/print_screen.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
-import 'package:http/http.dart' as http;
-import 'package:jallaliefern_taking_orders_app/services/secure_storage_service.dart';
-import 'package:path/path.dart';
+import 'dart:typed_data';
 import 'package:intl/intl.dart';
-import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:oktoast/oktoast.dart';
+import 'package:jallaliefern_taking_orders_app/services/secure_storage_service.dart';
 import 'package:jallaliefern_taking_orders_app/models/restaurant.dart';
 import 'package:jallaliefern_taking_orders_app/utils/service_locator.dart';
 import '../models/order.dart';
-import 'dart:async';
 import 'package:jallaliefern_taking_orders_app/Constants.dart';
-import 'package:blue_thermal_printer/blue_thermal_printer.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:flutter/services.dart';
 
 class PrintScreen extends StatefulWidget {
   final Order order;
@@ -23,263 +24,260 @@ class PrintScreen extends StatefulWidget {
 }
 
 class _PrintScreenState extends State<PrintScreen> {
-  BlueThermalPrinter bluetooth = BlueThermalPrinter.instance;
+  PrinterBluetoothManager printerManager = PrinterBluetoothManager();
+  List<PrinterBluetooth> _devices = [];
 
-  List<BluetoothDevice> _devices = [];
-  BluetoothDevice? _device;
-  // ignore: unused_field
-  bool _connected = false;
-  // ignore: unused_field
-  bool _pressed = false;
-  String? pathImage;
   @override
   void initState() {
     super.initState();
-    initSavetoPath();
-    initPlatformState();
-  }
 
-  initSavetoPath() async {
-    var response = await http.get(Uri.parse(locator<Restaurant>().logo!));
-    Directory documentDirectory = await getApplicationDocumentsDirectory();
-    File file = new File(join(documentDirectory.path, 'logo.png'));
-    await file.writeAsBytes(response.bodyBytes);
-    setState(() {
-      pathImage = file.path;
-    });
-  }
-
-  Future<void> initPlatformState() async {
-    List<BluetoothDevice> devices = [];
-
-    try {
-      devices = await bluetooth.getBondedDevices();
-    } on PlatformException {
-      print("PLATFORM ERROR");
-    }
-
-    bluetooth.onStateChanged().listen((state) {
-      switch (state) {
-        case BlueThermalPrinter.CONNECTED:
-          setState(() {
-            _connected = true;
-            _pressed = false;
-          });
-          break;
-        case BlueThermalPrinter.DISCONNECTED:
-          setState(() {
-            _connected = false;
-            _pressed = false;
-          });
-          break;
-        default:
-          print(state);
-          break;
-      }
-    });
-    if (!mounted) return;
-    setState(() {
-      _devices = devices;
-    });
-  }
-
-  List<DropdownMenuItem<BluetoothDevice>> _getDeviceItems() {
-    List<DropdownMenuItem<BluetoothDevice>> items = [];
-    if (_devices.isEmpty) {
-      items.add(DropdownMenuItem(
-        child: Text('NONE'),
-      ));
-    } else {
-      _devices.forEach((device) {
-        items.add(DropdownMenuItem(
-          child: Text(device.name ?? ""),
-          value: device,
-        ));
+    printerManager.scanResults.listen((devices) async {
+      // print('UI: Devices found ${devices.length}');
+      setState(() {
+        _devices = devices;
       });
-    }
-    return items;
+    });
   }
 
-  void _connect() {
-    if (_device == null) {
-      show('No device selected.');
-    } else {
-      bluetooth.isConnected.then((isConnected) {
-        if (!isConnected!) {
-          bluetooth.connect(_device!).catchError((error) {
-            show('error occured while connecting to device');
-            print("AAAA $error");
-            setState(() => _pressed = false);
-          });
-          setState(() => _pressed = true);
-        }
-      });
-    }
+  void _startScanDevices() {
+    setState(() {
+      _devices = [];
+    });
+    printerManager.startScan(Duration(seconds: 4));
   }
 
-  void _disconnect() {
-    bluetooth.disconnect();
-    setState(() => _pressed = true);
+  void _stopScanDevices() {
+    printerManager.stopScan();
   }
 
-  Future show(
-    String message, {
-    Duration duration: const Duration(seconds: 3),
-  }) async {
-    await new Future.delayed(new Duration(milliseconds: 100));
-    ScaffoldMessenger.of(this.context).showSnackBar(
-      new SnackBar(
-        content: new Text(
-          message,
-          style: new TextStyle(
-            color: Colors.white,
-          ),
+  Future<Ticket> orderReceipt(PaperSize paper) async {
+    final Ticket ticket = Ticket(paper);
+
+    // Print image
+    // final ByteData data = await rootBundle.load('assets/rabbit_black.jpg');
+    //final Uint8List bytes = data.buffer.asUint8List();
+    //final Image image = decodeImage(bytes);
+    // ticket.image(image);
+
+    final now = DateTime.now();
+    final formatter = DateFormat('MM/dd/yyyy H:m');
+    final String timestamp = formatter.format(now);
+    ticket.text(timestamp, styles: PosStyles(align: PosAlign.center));
+
+    ticket.text(locator<Restaurant>().name!,
+        styles: PosStyles(
+          align: PosAlign.center,
+          height: PosTextSize.size2,
+          width: PosTextSize.size2,
         ),
-        duration: duration,
-      ),
-    );
+        linesAfter: 1);
+
+    ticket.text(
+        '${locator<Restaurant>().street}, ${locator<Restaurant>().city}, ${locator<Restaurant>().country}',
+        styles: PosStyles(align: PosAlign.center));
+
+    ticket.text('Tel1: ${locator<Restaurant>().phone1 ?? ""}',
+        styles: PosStyles(align: PosAlign.center));
+    ticket.text('Tel2: ${locator<Restaurant>().phone2 ?? ""}',
+        styles: PosStyles(align: PosAlign.center));
+
+    ticket.text(
+        'Web: ${Uri.parse(await locator<SecureStorageService>().apiUrl).host}',
+        styles: PosStyles(align: PosAlign.center),
+        linesAfter: 1);
+    print("AAAAAAAAAA");
+    ticket.hr(ch: '=');
+    ticket.row([
+      PosColumn(text: 'Full Name:', width: 4),
+      PosColumn(
+          text: "${widget.order.firstName} ${widget.order.lastName}", width: 8)
+    ]);
+    ticket.row([
+      PosColumn(text: 'Order ID:', width: 4),
+      PosColumn(text: "#${widget.order.id}", width: 8)
+    ]);
+    ticket.row([
+      PosColumn(text: 'Order Slug:', width: 4),
+      PosColumn(text: "${widget.order.slug}", width: 8)
+    ]);
+    if (widget.order.delivery != null) {
+      if (await widget.order.delivery!.getSection() != null) {
+        ticket.row([
+          PosColumn(text: 'Full Address:', width: 4),
+          PosColumn(
+              text:
+                  "${(await widget.order.delivery!.getZone())!.name}, ${(await widget.order.delivery!.getSection())!.name}, ${widget.order.delivery!.address}",
+              width: 8)
+        ]);
+      } else {
+        ticket.row([
+          PosColumn(text: 'Full Address:', width: 4),
+          PosColumn(
+              text:
+                  "${(await widget.order.delivery!.getZone())!.name}, ${widget.order.delivery!.address}",
+              width: 8)
+        ]);
+      }
+    }
+    ticket.text('Tel1: ${widget.order.phone}',
+        styles: PosStyles(align: PosAlign.center));
+
+    ticket.hr();
+
+    for (int i = 0; i < widget.order.items.length; i++) {
+      var item = widget.order.items[i];
+      ticket.row([
+        PosColumn(text: '${item.quantity}x', width: 1),
+        (await item.getMealSize() == null)
+            ? PosColumn(text: '${(await item.getMeal())!.name}', width: 9)
+            : PosColumn(
+                text:
+                    '${(await item.getMeal())!.name} - ${(await item.getMealSize())!.name}',
+                width: 9),
+        PosColumn(
+            text: '${item.totalPrice}',
+            width: 2,
+            styles: PosStyles(align: PosAlign.right)),
+      ]);
+      for (int j = 0; j < item.addons!.length; j++) {
+        var itemAddon = item.addons![j];
+        ticket.row([
+          PosColumn(width: 3),
+          PosColumn(text: '>${(await itemAddon.getAddon())!.name}', width: 7),
+          PosColumn(
+              text: '${itemAddon.totalPrice}',
+              width: 2,
+              styles: PosStyles(align: PosAlign.right)),
+        ]);
+      }
+    }
+
+    ticket.hr();
+
+    ticket.row([
+      PosColumn(
+          text: 'TOTAL',
+          width: 6,
+          styles: PosStyles(
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+      PosColumn(
+          text: '${widget.order.totalPrice}${locator<Restaurant>().currency}',
+          width: 6,
+          styles: PosStyles(
+            align: PosAlign.right,
+            height: PosTextSize.size2,
+            width: PosTextSize.size2,
+          )),
+    ]);
+
+    ticket.hr(ch: '=', linesAfter: 1);
+
+    ticket.feed(2);
+    ticket.text('Thank you!',
+        styles: PosStyles(align: PosAlign.center, bold: true));
+
+    // Print QR Code from image
+    // try {
+    //   const String qrData = 'example.com';
+    //   const double qrSize = 200;
+    //   final uiImg = await QrPainter(
+    //     data: qrData,
+    //     version: QrVersions.auto,
+    //     gapless: false,
+    //   ).toImageData(qrSize);
+    //   final dir = await getTemporaryDirectory();
+    //   final pathName = '${dir.path}/qr_tmp.png';
+    //   final qrFile = File(pathName);
+    //   final imgFile = await qrFile.writeAsBytes(uiImg.buffer.asUint8List());
+    //   final img = decodeImage(imgFile.readAsBytesSync());
+
+    //   ticket.image(img);
+    // } catch (e) {
+    //   print(e);
+    // }
+
+    // Print QR Code using native function
+    // ticket.qrcode('example.com');
+
+    ticket.feed(2);
+    ticket.cut();
+    return ticket;
   }
 
-  void _testPrint() async {
-    //SIZE
-    // 0- normal size text
-    // 1- only bold text
-    // 2- bold with medium text
-    // 3- bold with large text
-    //ALIGN
-    // 0- ESC_ALIGN_LEFT
-    // 1- ESC_ALIGN_CENTER
-    // 2- ESC_ALIGN_RIGHT
-    bluetooth.isConnected.then((isConnected) async {
-      if (isConnected!) {
-        // HEADER
-        // bluetooth.printImage(pathImage!);
-        bluetooth.printCustom(locator<Restaurant>().name ?? "", 3, 1);
-        bluetooth.printNewLine();
-        bluetooth.printCustom(
-            DateFormat('yyyy-MM-dd - kk:mm').format(DateTime.now()), 0, 2);
-        bluetooth.printNewLine();
-        bluetooth.printCustom(
-            "${locator<Restaurant>().street}, ${locator<Restaurant>().city}, ${locator<Restaurant>().country}",
-            1,
-            1);
-        bluetooth.printNewLine();
-        bluetooth.printCustom(
-            '${locator<Restaurant>().phone1 ?? ""} - ${locator<Restaurant>().phone2 ?? ""}',
-            1,
-            1);
-        bluetooth.printNewLine();
-        bluetooth.printCustom(
-            Uri.parse(await locator<SecureStorageService>().apiUrl).host, 1, 1);
+  void _testPrint(PrinterBluetooth printer) async {
+    printerManager.selectPrinter(printer);
 
-        bluetooth.printNewLine();
-        bluetooth.printCustom("-" * 20, 3, 1);
-        bluetooth.printNewLine();
+    const PaperSize paper = PaperSize.mm80;
+    final PosPrintResult res =
+        await printerManager.printTicket(await orderReceipt(paper));
 
-        // Order Details
-        bluetooth.printCustom("Order Details", 3, 1);
-        bluetooth.printNewLine();
-
-        bluetooth.printCustom("#${widget.order.id}", 2, 0);
-        bluetooth.printNewLine();
-        bluetooth.printCustom(
-            "Full Name: ${widget.order.firstName} ${widget.order.lastName}",
-            1,
-            0);
-        if (widget.order.delivery != null) {
-          if ((await widget.order.delivery!.getSection()) == null) {
-            bluetooth.printCustom(
-                "Address: ${(await widget.order.delivery!.getZone())!.name}, ${(await widget.order.delivery!.getSection())!.name}, ${widget.order.delivery!.address}",
-                1,
-                0);
-          } else {
-            bluetooth.printCustom(
-                "Address: ${(await widget.order.delivery!.getZone())!.name}, ${widget.order.delivery!.address}",
-                1,
-                0);
-          }
-          bluetooth.printNewLine();
-        }
-
-        bluetooth.printCustom("Phone: ${widget.order.phone}", 1, 0);
-        bluetooth.printNewLine();
-
-        // ITEMS
-        bluetooth.printCustom("Items:", 3, 1);
-        for (int i = 0; i < widget.order.items.length; i++) {
-          var item = widget.order.items[i];
-          if (item.size != null) {
-            bluetooth.printLeftRight(
-                "${(await item.getMeal())!.name} - ${item.size!.name} x${item.quantity}",
-                "${item.totalPrice} ${locator<Restaurant>().currency}",
-                2);
-          } else {
-            bluetooth.printLeftRight(
-                "${(await item.getMeal())!.name} x${item.quantity}",
-                "${item.totalPrice} ${locator<Restaurant>().currency}",
-                2);
-          }
-
-          if (item.addons != null) {
-            for (int j = 0; j < item.addons!.length; j++) {
-              var itemAddon = item.addons![j];
-              bluetooth.printLeftRight(
-                  "    >${(await itemAddon.getAddon())!.name}",
-                  "${itemAddon.totalPrice} ${locator<Restaurant>().currency}    ",
-                  1);
-            }
-          }
-          bluetooth.printNewLine();
-        }
-
-        bluetooth.printNewLine();
-        bluetooth.printCustom(
-            "${widget.order.totalPrice} ${locator<Restaurant>().currency}",
-            3,
-            2);
-        bluetooth.printNewLine();
-        bluetooth.paperCut();
-      } else {
-        show("connect to device first");
-      }
-    });
+    showToast(res.msg);
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Kcolor,
         title: Text("Print Order #${widget.order.id}"),
       ),
-      body: Column(
-        children: [
-          Row(children: [
-            Text('Choose Device: '),
-            DropdownButton<BluetoothDevice>(
-              value: _device,
-              icon: const Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              style: const TextStyle(color: Colors.deepPurple),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
+      body: ListView.builder(
+          itemCount: _devices.length,
+          itemBuilder: (BuildContext context, int index) {
+            return InkWell(
+              onTap: () => _testPrint(_devices[index]),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 60,
+                    padding: EdgeInsets.only(left: 10),
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.print),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(_devices[index].name ?? ''),
+                              Text(_devices[index].address),
+                              Text(
+                                'Click to print a test receipt',
+                                style: TextStyle(color: Colors.grey[700]),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                ],
               ),
-              onChanged: (BluetoothDevice? newValue) {
-                setState(() {
-                  _device = newValue!;
-                });
-              },
-              items: _getDeviceItems(),
-            ),
-          ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(onPressed: _connect, child: Text('Connect')),
-              ElevatedButton(onPressed: _testPrint, child: Text('Print')),
-            ],
-          )
-        ],
-      ));
+            );
+          }),
+      floatingActionButton: StreamBuilder<bool>(
+        stream: printerManager.isScanningStream,
+        initialData: false,
+        builder: (c, snapshot) {
+          if (snapshot.data!) {
+            return FloatingActionButton(
+              child: Icon(Icons.stop),
+              onPressed: _stopScanDevices,
+              backgroundColor: Colors.red,
+            );
+          } else {
+            return FloatingActionButton(
+              child: Icon(Icons.search),
+              onPressed: _startScanDevices,
+            );
+          }
+        },
+      ),
+    );
+  }
 }
