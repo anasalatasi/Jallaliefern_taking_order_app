6e5496bbc07f0444f45eeeba8365112cf9137053
mousasya3 <mousasya3@gmail.com>
2021-08-26 00:16:26 +0300

    fixed encoding

--- lib/screens/print_screen.dart
+++ lib/screens/print_screen.dart
@@ -8,12 +8,16 @@ import 'package:image/image.dart';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:esc_pos_bluetooth/esc_pos_bluetooth.dart';
 import 'package:flutter/material.dart' hide Image;
+import 'dart:convert' show utf8;
 import 'package:oktoast/oktoast.dart';
 import 'package:jallaliefern_taking_orders_app/services/secure_storage_service.dart';
 import 'package:jallaliefern_taking_orders_app/models/restaurant.dart';
 import 'package:jallaliefern_taking_orders_app/utils/service_locator.dart';
 import '../models/order.dart';
 import 'package:jallaliefern_taking_orders_app/Constants.dart';
+import 'package:charset_converter/charset_converter.dart';
+import 'dart:typed_data';
+import 'package:flutter_charset_detector/flutter_charset_detector.dart';
 
 class PrintScreen extends StatefulWidget {
   final Order order;
@@ -46,6 +50,7 @@ class _PrintScreenState extends State<PrintScreen> {
     final now = DateTime.now();
     final formatter = DateFormat('MM/dd/yyyy H:m');
     final String timestamp = formatter.format(now);
+
     ticket.text(timestamp,
         styles:
             PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur));
@@ -59,19 +64,25 @@ class _PrintScreenState extends State<PrintScreen> {
         linesAfter: 1);
 
     ticket.text(
-        '${locator<Restaurant>().street}, ${locator<Restaurant>().city}, ${locator<Restaurant>().country}',
-        styles:
-            PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur));
-
-    ticket.text('Tel1: ${locator<Restaurant>().phone1 ?? ""}',
-        styles:
-            PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur));
-    ticket.text('Tel2: ${locator<Restaurant>().phone2 ?? ""}',
+        String.fromCharCodes(await CharsetConverter.encode("windows1252",
+            '${locator<Restaurant>().street}, ${locator<Restaurant>().city}, ${locator<Restaurant>().country}')),
         styles:
             PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur));
 
     ticket.text(
-        'Web: ${Uri.parse(await locator<SecureStorageService>().apiUrl).host}',
+        String.fromCharCodes(await CharsetConverter.encode(
+            "windows1252", 'Tel1: ${locator<Restaurant>().phone1 ?? ""}')),
+        styles:
+            PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur));
+    ticket.text(
+        String.fromCharCodes(await CharsetConverter.encode(
+            "windows1252", 'Tel2: ${locator<Restaurant>().phone2 ?? ""}')),
+        styles:
+            PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur));
+
+    ticket.text(
+        String.fromCharCodes(await CharsetConverter.encode("windows1252",
+            'Web: ${Uri.parse(await locator<SecureStorageService>().apiUrl).host}')),
         styles:
             PosStyles(align: PosAlign.center, codeTable: PosCodeTable.westEur),
         linesAfter: 1);
@@ -82,7 +93,9 @@ class _PrintScreenState extends State<PrintScreen> {
           width: 4,
           styles: PosStyles(codeTable: PosCodeTable.westEur)),
       PosColumn(
-          text: "${widget.order.firstName} ${widget.order.lastName}",
+          text: String.fromCharCodes(await CharsetConverter.encode(
+              "windows1252",
+              "${widget.order.firstName} ${widget.order.lastName}")),
           width: 8,
           styles: PosStyles(codeTable: PosCodeTable.westEur))
     ]);
@@ -114,8 +127,9 @@ class _PrintScreenState extends State<PrintScreen> {
               width: 4,
               styles: PosStyles(codeTable: PosCodeTable.westEur)),
           PosColumn(
-              text:
-                  "${(await widget.order.delivery!.getZone())!.name}, ${(await widget.order.delivery!.getSection())!.name}, ${widget.order.delivery!.address}",
+              text: String.fromCharCodes(await CharsetConverter.encode(
+                  "windows1252",
+                  "${(await widget.order.delivery!.getZone())!.name}, ${(await widget.order.delivery!.getSection())!.name}, ${widget.order.delivery!.address}")),
               width: 8,
               styles: PosStyles(codeTable: PosCodeTable.westEur))
         ]);
@@ -126,8 +140,9 @@ class _PrintScreenState extends State<PrintScreen> {
               width: 4,
               styles: PosStyles(codeTable: PosCodeTable.westEur)),
           PosColumn(
-              text:
-                  "${(await widget.order.delivery!.getZone())!.name}, ${widget.order.delivery!.address}",
+              text: String.fromCharCodes(await CharsetConverter.encode(
+                  "windows1252",
+                  "${(await widget.order.delivery!.getZone())!.name}, ${widget.order.delivery!.address}")),
               width: 8,
               styles: PosStyles(codeTable: PosCodeTable.westEur))
         ]);
@@ -148,12 +163,14 @@ class _PrintScreenState extends State<PrintScreen> {
             styles: PosStyles(codeTable: PosCodeTable.westEur)),
         (await item.getMealSize() == null)
             ? PosColumn(
-                text: '${(await item.getMeal())!.name}',
+                text: String.fromCharCodes(await CharsetConverter.encode(
+                    "windows1252", '${(await item.getMeal())!.name}')),
                 width: 9,
                 styles: PosStyles(codeTable: PosCodeTable.westEur))
             : PosColumn(
-                text:
-                    '${(await item.getMeal())!.name} - ${(await item.getMealSize())!.name}',
+                text: String.fromCharCodes(await CharsetConverter.encode(
+                    "windows1252",
+                    '${(await item.getMeal())!.name} - ${(await item.getMealSize())!.name}')),
                 width: 9,
                 styles: PosStyles(codeTable: PosCodeTable.westEur)),
         PosColumn(
@@ -168,7 +185,8 @@ class _PrintScreenState extends State<PrintScreen> {
           PosColumn(
               width: 3, styles: PosStyles(codeTable: PosCodeTable.westEur)),
           PosColumn(
-              text: '>${(await itemAddon.getAddon())!.name}',
+              text: String.fromCharCodes(await CharsetConverter.encode(
+                  "windows1252", '>${(await itemAddon.getAddon())!.name}')),
               width: 7,
               styles: PosStyles(codeTable: PosCodeTable.westEur)),
           PosColumn(
